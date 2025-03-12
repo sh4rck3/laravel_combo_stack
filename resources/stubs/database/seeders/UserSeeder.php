3e2076cad3ae5f650d98343c05bea7420ff153a5
Luccas Woiciechoski <sh4rck3@gmail.com>
2025-03-12 09:44:52 -0300

    alterado o namespace do modelsUser e do UserSeeder para testar a criação dos usuarios e tambem foi removido a publish do spatie pra nao gerar duas migration

--- resources/stubs/database/seeders/UserSeeder.php
+++ resources/stubs/database/seeders/UserSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
 
 class UserSeeder extends Seeder
 {
